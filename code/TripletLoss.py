f66addb10fab265fe26c556883a2d0d77e9e729d
danielt17 <60748408+danielt17@users.noreply.github.com>
2021-06-05 15:12:41 +0300

    Add files via upload

---
+++ code/TripletLoss.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  5 15:08:36 2021
+
+@author: danie
+"""
+
+# %% Imports
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# %% Triplet loss
+
+def tripletLoss(margin = 0.2, p=2):
+    triplet_loss = nn.TripletMarginLoss(margin=margin, p=p)
+    return triplet_loss
+
+# %% Main
+
+if __name__ == '__main__':
+    triplet_loss = tripletLoss()
+    anchor = torch.randn(100, 128, requires_grad=True)
+    positive = torch.randn(100, 128, requires_grad=True)
+    negative = torch.randn(100, 128, requires_grad=True)
+    output = triplet_loss(anchor, positive, negative)
+    output.backward()
